9e96c6d73b56918f3e95eb1e8692977720e5d03b
aumgn <aumgn@free.fr>
2012-05-24 11:07:15 +0200

    Add WorldTypeAdapterFactory

---
+++ gconf/src/main/java/fr/aumgn/bukkitutils/gconf/typeadapter/WorldTypeAdapterFactory.java
@@ -0,0 +1,58 @@
+package fr.aumgn.bukkitutils.gconf.typeadapter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import fr.aumgn.bukkitutils.gconf.GConfLoadRuntimeException;
+
+@SuppressWarnings("unchecked")
+public class WorldTypeAdapterFactory implements TypeAdapterFactory {
+
+    private final boolean throwsException;
+
+    public WorldTypeAdapterFactory() {
+        this(true);
+    }
+
+    public WorldTypeAdapterFactory(boolean throwsException) {
+        this.throwsException = throwsException;
+    }
+
+    @Override
+    public <T> TypeAdapter<T> create(final Gson gson, TypeToken<T> token) {
+        if (!World.class.isAssignableFrom(token.getRawType())) {
+            return null;
+        }
+
+        return new TypeAdapter<T>() {
+
+            @Override
+            public T read(JsonReader reader) throws IOException {
+                UUID uuid = gson.fromJson(reader, UUID.class);
+                World world = Bukkit.getWorld(uuid);
+                if (world == null && throwsException) {
+                    throw new GConfLoadRuntimeException("Unable to load world for uuid :" + uuid);
+                }
+
+                return (T) world;
+            }
+
+            @Override
+            public void write(JsonWriter writer, T obj) throws IOException {
+                World world = (World) obj;
+                gson.toJson(world.getUID(), UUID.class, writer);
+            }
+        };
+    }
+
+}
